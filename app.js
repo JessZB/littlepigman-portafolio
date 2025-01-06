aee687f77f3ee36b283e63c05fa0fb745ae8940e
Jesús Zapata <jesusnbz22@gmail.com>
2025-01-06 11:33:35 -0400

    eliminar función ping y su invocación en app.js

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ import scrollUp from './js/scrollUp.js'
 
 const d = document
 
-ping()
-
 d.addEventListener('DOMContentLoaded', (e) => {
   navBar('.nav-menu', '.activate-menu', '.activate-menu-btn', 'active')
   scrollUp('.up-btn', 'activate')
