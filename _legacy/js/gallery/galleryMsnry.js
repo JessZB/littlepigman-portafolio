473f373579871a0ce457f8558cc7bef7463c39af
jesuszapataDev <90283911+JessZB@users.noreply.github.com>
2026-04-25 11:01:45 -0400

    chore: add legacy snapshot and migrate to shared-agents submodule
    - _legacy/: snapshot at commit aee687f (original HTML portfolio v1)
    
    - _shared_agents: linked shared-agents repo as submodule

---
+++ _legacy/js/gallery/galleryMsnry.js
@@ -0,0 +1,28 @@
+
+var elem = document.querySelector('.grid-container');
+var msnry = new Masonry(elem, {
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: 220,
+    gutter: 20,
+    fitWidth: true,
+});
+
+var elem2 = document.querySelector('.grid-container2');
+var msnry2 = new Masonry(elem2, {
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: 220,
+    gutter: 20,
+    fitWidth: true,
+});
+
+var elem3 = document.querySelector('.grid-container3');
+var msnry3 = new Masonry(elem3, {
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: 220,
+    gutter: 20,
+    fitWidth: true,
+});
+
